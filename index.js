a17d2858876c36d4e70244d546f2b9f8dba61dd3
Spencer Lindemuth <lindemut@gmail.com>
2019-02-27 20:17:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  var element = document.querySelector(selector)
+  return element
+}
 
+function nestedTarget(){
+  var element = document.querySelector('#nested div.target')
+  return element
+}
+
+function increaseRankBy(n){
+  var num = parseInt(n, 10)
+  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < rank.length; i++) {
+    var first = parseInt(rank[i].innerHTML, 10)
+    rank[i].innerHTML = (first + num).toString();
+  }
+}
+
+function deepestChild(){
+  var div = document.querySelectorAll(`#grand-node div`);
+  return div[div.length-1];
+
+}
